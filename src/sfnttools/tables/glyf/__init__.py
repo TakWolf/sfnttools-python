5e1fb46ccaed37b941859322ea536ed3707d43c7
TakWolf <takwolf@foxmail.com>
2025-05-08 16:30:40 +0800

    a

--- src/sfnttools/tables/glyf/__init__.py
+++ src/sfnttools/tables/glyf/__init__.py
@@ -13,38 +13,38 @@ class GlyfTable(SfntTable):
     def parse(data: bytes, reader: SfntTableReader) -> 'GlyfTable':
         loca_table: LocaTable = reader.get_or_parse_table('loca')
 
-        glyph_descriptions = []
+        glyphs = []
         for i in range(len(loca_table.offsets) - 1):
             offset = loca_table.offsets[i]
             next_offset = loca_table.offsets[i + 1]
             glyph_data = data[offset:next_offset]
             if glyph_data == b'':
-                glyph_description = None
+                glyph = None
             else:
                 num_contours = int.from_bytes(glyph_data[0:2], 'big', signed=True)
                 if num_contours > 0:
-                    glyph_description = SimpleGlyph.parse(glyph_data)
+                    glyph = SimpleGlyph.parse(glyph_data)
                 elif num_contours < 0:
-                    glyph_description = ComponentGlyph.parse(glyph_data)
+                    glyph = ComponentGlyph.parse(glyph_data)
                 else:
                     raise SfntError('[glyf] bad glyph data')
-            glyph_descriptions.append(glyph_description)
+            glyphs.append(glyph)
 
-        return GlyfTable(glyph_descriptions)
+        return GlyfTable(glyphs)
 
-    glyph_descriptions: list[SimpleGlyph | ComponentGlyph | None]
+    glyphs: list[SimpleGlyph | ComponentGlyph | None]
 
     def __init__(
             self,
-            glyph_descriptions: list[SimpleGlyph | ComponentGlyph | None] | None = None,
+            glyphs: list[SimpleGlyph | ComponentGlyph | None] | None = None,
     ):
-        self.glyph_descriptions = [] if glyph_descriptions is None else glyph_descriptions
+        self.glyphs = [] if glyphs is None else glyphs
 
     def copy(self) -> 'GlyfTable':
-        glyph_descriptions = []
-        for glyph_description in self.glyph_descriptions:
-            glyph_descriptions.append(None if glyph_description is None else glyph_description.copy())
-        return GlyfTable(glyph_descriptions)
+        glyphs = []
+        for glyph in self.glyphs:
+            glyphs.append(None if glyph is None else glyph.copy())
+        return GlyfTable(glyphs)
 
     def dump(self, writer: SfntTableWriter) -> bytes:
         buffer = BytesIO()
