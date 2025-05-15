92098db3261d3a2a1dcec7199f6509fd4c31166c
TakWolf <takwolf@foxmail.com>
2025-05-15 21:38:52 +0800

    a

--- src/sfnttools/table.py
+++ src/sfnttools/table.py
@@ -5,7 +5,9 @@ from sfnttools.configs import SfntConfigs
 
 class SfntTable:
     parse_dependencies: list[str] = []
+    update_dependencies: list[str] = []
     dump_dependencies: list[str] = []
+    dump_generates: list[str] = []
 
     @staticmethod
     @abstractmethod
@@ -15,6 +17,9 @@ class SfntTable:
     @abstractmethod
     def copy(self) -> 'SfntTable':
         raise NotImplementedError()
+
+    def update(self, configs: SfntConfigs, dependencies: dict[str, 'SfntTable']):
+        pass
 
     @abstractmethod
     def dump(self, configs: SfntConfigs, dependencies: dict[str, 'SfntTable']) -> tuple[bytes, dict[str, 'SfntTable']]:
